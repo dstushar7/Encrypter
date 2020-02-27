cbbce6f0b40f8fb6e5e1a6322595c2b405a11228
tusharsarker97 <tusharsarker97@gmail.com>
2020-02-28 04:05:40 +0600

    Ciphering program is finished.
    Work of 2hours. Learned Dictionaries

--- Transpositional Cipher.py
+++ Transpositional Cipher.py
@@ -1,25 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+def ctos(x):#convert to required structure
+    x = x.replace(" ","")#remove white spaces
+    x = x.lower()#convert to lowercase
+    return x
 
-This is a temporary script file."""
 import numpy as np
+enk = str(input("Enter Encryption Key with no repeated letters: "))
 msg = str(input("Enter your message: "))
-msg = msg.replace(" ","") #removing white spaces
-msg = msg.lower() #converting to all lower case
-l = len(msg)
-m = int(l/6)
-n = l%6
-for i in range(0,6-n):
-    msg = msg+'a'#fill the matrix
-msg = list(msg) #convereting to list
-matrix = np.array(msg).reshape(m+1,6)
-#Converting list to a numpy matrix
-#positional matrix
+enk = ctos(enk)
+msg = ctos(msg)
+lenk= len(enk)
+lenm= len(msg)
+m = int(lenm/lenk)#To know the number of rows
+n = lenm%lenk
+for i in range(lenk-n):
+    msg = msg+'a'
+msg = list(msg)
+enk = list(enk)
+matrix = np.array(msg).reshape(m+1,lenk) #making the matrix of the message
+matrix = matrix.T #Transpose the matrix to point the indexes to a dictionary's key
+keyd = dict()
+for i in range(lenk):
+    keyd[enk[i]] = matrix[i] #set columns as key's letter
+enk.sort() #sorting the key
 a = []
-for i in range(6):
+for i in enk:
+    a.append(keyd[i]) #taking the sorted columns along with dictionary values
+matrix = np.array(a) #taking numpy matrix to easily access
+a = []
+for i in range(lenk):
     for j in range(m+1):
-        a.append(matrix[j][i])
-c = ''.join(str(e) for e in a)#converting list into string
+        a.append(matrix[i][j]) #take the cipher text list as output
+c = ''.join(str(e) for e in a) #convert to string
 print(c)
-    

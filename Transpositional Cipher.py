a23f0a96a4e492af2b860a397a2204dcf2bef96d
dstushar7 <35169303+dstushar7@users.noreply.github.com>
2021-07-04 00:06:15 +0600

    Loop 10 times

--- Transpositional Cipher.py
+++ Transpositional Cipher.py
@@ -4,31 +4,34 @@ def ctos(x):#convert to required structure
     return x
 
 import numpy as np
-enk = str(input("Enter Encryption Key with no repeated letters: "))
-msg = str(input("Enter your message: "))
-enk = ctos(enk)
-msg = ctos(msg)
-lenk = len(enk)
-lenm = len(msg)
-m = int(lenm/lenk)#To know the number of rows
-n = lenm%lenk
-for i in range(lenk-n):
-    msg = msg+'a'
-msg = list(msg)
-enk = list(enk)
-matrix = np.array(msg).reshape(m+1,lenk) #making the matrix of the message
-matrix = matrix.T #Transpose the matrix to point the indexes to a dictionary's key
-keyd = dict()
-for i in range(lenk):
-    keyd[enk[i]] = matrix[i] #set columns as key's letter
-enk.sort() #sorting the key
-a = []
-for i in enk:
-    a.append(keyd[i]) #taking the sorted columns along with dictionary values
-matrix = np.array(a) #taking numpy matrix to easily access
-a = []
-for i in range(lenk):
-    for j in range(m+1):
-        a.append(matrix[i][j]) #take the cipher text list as output
-c = ''.join(str(e) for e in a) #convert to string
-print(c)
+x = 1
+while x < 10: #Take Inputs 10 times
+    enk = str(input("Enter Encryption Key with no repeated letters: "))
+    msg = str(input("Enter your message: "))
+    enk = ctos(enk)
+    msg = ctos(msg)
+    lenk = len(enk)
+    lenm = len(msg)
+    m = int(lenm/lenk)#To know the number of rows
+    n = lenm%lenk
+    for i in range(lenk-n):
+        msg = msg+'a'
+    msg = list(msg)
+    enk = list(enk)
+    matrix = np.array(msg).reshape(m+1,lenk) #making the matrix of the message
+    matrix = matrix.T #Transpose the matrix to point the indexes to a dictionary's key
+    keyd = dict()
+    for i in range(lenk):
+        keyd[enk[i]] = matrix[i] #set columns as key's letter
+    enk.sort() #sorting the key
+    a = []
+    for i in enk:
+        a.append(keyd[i]) #taking the sorted columns along with dictionary values
+    matrix = np.array(a) #taking numpy matrix to easily access
+    a = []
+    for i in range(lenk):
+        for j in range(m+1):
+            a.append(matrix[i][j]) #take the cipher text list as output
+    c = ''.join(str(e) for e in a) #convert to string
+    print(c)
+    x += 1
